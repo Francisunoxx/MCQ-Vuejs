b2b9a893dffecd82ee40939116ea70615c1916e1
Francisunoxx <francisunoxx@gmail.com>
2019-10-21 12:49:46 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      janella: 'maganda',
+    };
+  },
+  methods: {
+    reload() {
+    },
+  },
+};
+</script>
+
+<style scoped src='./css/App.css'/>
---
+++ src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Ecosystem</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      msg: ""
+    };
+  }
+};
+</script>
+
+<style scoped src='../css/Login.css'/>
